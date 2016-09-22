0fa1e3998cd1f575b867b81c2482b16dbc43a6c9
brown2206 <brown.angelo.jr@gmail.com>
2016-09-22 09:58:00 -0400

    move jordan

--- catdogpanda.js
+++ catdogpanda.js
@@ -1,5 +1,14 @@
 $("body").mousemove(function(event){
     var position = $("#coords").text(event.pageX + ", " + event.pageY);
-    console.log(event.pageX);
-    console.log(even.pageY);
+    var x = event.pageX;
+    var y = event.pageY;
+    console.log(x,y);
+    $jordan.css({
+      'margin-left': x / 2,
+      'margin-top': y / 2
+    })
 });
+
+var $jordan = $(".jordan");
+var $dog = $(".dog");
+var $panda = $(".panda");
